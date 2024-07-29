2fcd3c14e84516967fe9ad2036bd16afd56f6c0d
unknown <kendrick_sin@hotmail.com>
2024-07-29 15:05:31 +0800

    update links

--- api.py
+++ api.py
@@ -5,7 +5,7 @@ from swap import swap
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the FastAPI server")
     parser.add_argument("--host", default="0.0.0.0", help="Host to run the server on")
-    parser.add_argument("--port", type=int, default=5000, help="Port to run the server on")
+    parser.add_argument("--port", type=int, default=8000, help="Port to run the server on")
     args = parser.parse_args()
 
     uvicorn.run(swap, host=args.host, port=args.port)
